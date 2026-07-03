1262d6db602de63664bfd9ba4c0226e2c1f564b4
Jiaming Pei <jiamingpei16@gmail.com>
2026-07-03 03:58:16 -0700

    feat(reports): add supporter entry flow (Home card + person picker)

--- App.tsx
+++ App.tsx
@@ -25,6 +25,7 @@ import SettingsScreen from './src/screens/SettingsScreen';
 import CategoriesScreen from './src/screens/categories/CategoriesScreen';
 import ReportsScreen from './src/screens/reports/ReportsScreen';
 import ReportViewScreen from './src/screens/reports/ReportViewScreen';
+import SelectPersonScreen from './src/screens/reports/SelectPersonScreen';
 import AllTasksScreen from './src/screens/tasks/AllTasksScreen';
 import CreateTaskScreen from './src/screens/tasks/CreateTaskScreen';
 import CreateTaskStepScreen from './src/screens/tasks/CreateTaskStepScreen';
@@ -103,6 +104,7 @@ function RootStack() {
           />
           <Stack.Screen name="CreateTaskStep" component={CreateTaskStepScreen} />
           <Stack.Screen name="ReorderSteps" component={ReorderStepsScreen} />
+          <Stack.Screen name="ReportPeople" component={SelectPersonScreen} />
           <Stack.Screen name="Reports" component={ReportsScreen} />
           <Stack.Screen name="ReportView" component={ReportViewScreen} />
         </>
